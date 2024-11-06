41ba6f455da9bfd9e9c1e643dcbdadeb642eb775
Daniel Sjöholm <Danielblomqvistsjoholm@gmail.com>
2024-11-06 22:02:12 +0100

    refactor app.py

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-from flask import Flask, render_template, jsonify, request
-from helpers.get_data import _get_trafikverket_data
+from datetime import datetime, timedelta
+from collections import Counter
+import requests
 
-app = Flask(__name__)
+def _get_trafikverket_data(start_time=None):
+    url = 'https://api.trafikinfo.trafikverket.se/v2/data.json'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    if start_time:
+        start_time = datetime.strptime(start_time, "%Y-%m-%d")
 
-@app.route('/get_data')
-def get_data():
-    try:
-        data = _get_trafikverket_data()
-        if data:
-            return jsonify({
-                "total_situations": data["total_situations"],
-                "message_type_counts": data["message_type_counts"]
-            })
-        else:
-            print('No data found')
-            return jsonify({"error": "Kunde inte hämta data"}), 500
-    except Exception as e:
-        print(f'Error occurred: {e}')
-        return jsonify({"error": "An error occurred while processing your request."}), 500
+    payload = """
+    <REQUEST>
+        <LOGIN authenticationkey='5c341f5a9438482ca26b412a7c146cea' />
+        <QUERY objecttype='Situation' schemaversion='1.5'>
+            <INCLUDE>Deviation</INCLUDE>
+        </QUERY>
+    </REQUEST>
+    """
+    
+    response = requests.post(url, data=payload, headers={'Content-Type': 'text/xml'})
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    if response.status_code == 200:
+        filtered_data = []
+        data = response.json()
+
+        total_situations = 0
+        message_type_counter = Counter()
+
+        for result in data['RESPONSE']['RESULT']:
+            for situation in result.get('Situation', []):
+                for deviation in situation.get('Deviation', []):
+                    deviation_start_time_str = deviation.get("StartTime", "")
+                    if deviation_start_time_str:
+                        deviation_start_time = datetime.strptime(deviation_start_time_str, "%Y-%m-%dT%H:%M:%S.%f%z").replace(tzinfo=None)
+                        if start_time and deviation_start_time >= start_time:
+                            filtered_data.append(deviation)
+                            total_situations += 1
+                            message_type_counter[deviation.get("MessageType", "Okänd")] += 1
+        return {
+            "filtered_data": filtered_data,
+            "total_situations": total_situations,
+            "message_type_counts": message_type_counter
+        }
+    else:
+        print(f"Fel vid hämtning av data: {response.status_code}")
+        return None
