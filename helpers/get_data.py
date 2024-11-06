fb25fd016d44b362f6df8b8ada14bf62be2bdaff
Daniel Sjöholm <Danielblomqvistsjoholm@gmail.com>
2024-11-06 22:58:59 +0100

    splitted get_data function into 2 functions

--- helpers/get_data.py
+++ helpers/get_data.py
@@ -1,46 +1,24 @@
-from datetime import datetime, timedelta
-from collections import Counter
 import requests
+import os
+from dotenv import load_dotenv
 
-def _get_trafikverket_data(start_time=None):
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+def get_trafikverket_data():
     url = 'https://api.trafikinfo.trafikverket.se/v2/data.json'
-
-    if start_time:
-        start_time = datetime.strptime(start_time, "%Y-%m-%d")
-
-    payload = """
+    payload = f"""
     <REQUEST>
-        <LOGIN authenticationkey='5c341f5a9438482ca26b412a7c146cea' />
+        <LOGIN authenticationkey='{api_key}' />
         <QUERY objecttype='Situation' schemaversion='1.5'>
             <INCLUDE>Deviation</INCLUDE>
         </QUERY>
     </REQUEST>
     """
-
     response = requests.post(url, data=payload, headers={'Content-Type': 'text/xml'})
 
     if response.status_code == 200:
-        filtered_data = []
-        data = response.json()
-
-        total_situations = 0
-        message_type_counter = Counter()
-
-        for result in data['RESPONSE']['RESULT']:
-            for situation in result.get('Situation', []):
-                for deviation in situation.get('Deviation', []):
-                    deviation_start_time_str = deviation.get("StartTime", "")
-                    if deviation_start_time_str:
-                        deviation_start_time = datetime.strptime(deviation_start_time_str, "%Y-%m-%dT%H:%M:%S.%f%z").replace(tzinfo=None)
-                        if start_time and deviation_start_time >= start_time:
-                            filtered_data.append(deviation)
-                            total_situations += 1
-                            message_type_counter[deviation.get("MessageType", "Okänd")] += 1
-        return {
-            "filtered_data": filtered_data,
-            "total_situations": total_situations,
-            "message_type_counts": message_type_counter
-        }
+        return response.json()
     else:
         print(f"Fel vid hämtning av data: {response.status_code}")
         return None
